52bc9e69c3d624d71fc47617a877bb772b649e0e
Rosiej <mateusz.rosiejka@gmail.com>
2017-11-05 12:32:18 +0100

    done

--- src/pl/sdacademy/java7krkr/Main.java
+++ src/pl/sdacademy/java7krkr/Main.java
@@ -1,8 +1,22 @@
 package pl.sdacademy.java7krkr;
 
+import java.util.LinkedList;
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        SoundMaker cat = new Cat();
+        SoundMaker dog = new Dog();
+        SoundMaker girrafe = new Giraffe();
+
+        List<SoundMaker> animals = new LinkedList<>();
+        animals.add(cat);
+        animals.add(dog);
+        animals.add(girrafe);
+
+        for (SoundMaker animal : animals) {
+            animal.makeSound();
+        }
     }
 }
